e71782bd005fc8245e3c0cc813f7c9979e512d20
j <xxx@gmail.com>
2016-12-14 21:43:27 +0800

    首页完成

--- js/index.js
+++ js/index.js
@@ -37,3 +37,56 @@ shezhi.onmouseover = function(){
 sousuoul.onmouseout = function(){
   sousuoul.style.display = "none";
 }
+
+//登录页
+var dl = document.getElementById("dl");
+var dianji_img = document.getElementById('dianji_img');
+var opacity = document.getElementById("opacity_div");
+var dl_div = document.getElementById("dl_div");
+var erweima_div = document.getElementById("erweima_div");
+dl.onclick = function(){
+  dl_div.style.display = 'block';
+  opacity.style.display = 'block';
+}
+dianji_img.onclick = function(){
+  dl_div.style.display = 'none';
+  opacity.style.display = 'none';
+  erweima_div.style.display = 'none';
+}
+
+//登录页里边的内容
+var user_dl = document.getElementById("user_dl");
+var password_dl = document.getElementById("password_dl");
+var user = document.getElementById('user');
+var pwd = document.getElementById("pwd");
+var user_img = document.getElementById("user_img");
+var suo_img = document.getElementById("suo_img");
+user.onfocus = function(){
+  user_dl.style.border = '1px solid #38f';
+  user_img.src = 'img/index/user1.png'
+}
+user.onblur = function(){
+  user_dl.style.border = '1px solid #ddd';
+  user_img.src = 'img/index/user.png'
+}
+pwd.onfocus = function(){
+  password_dl.style.border = '1px solid #38f';
+  suo_img.src = 'img/index/suo1.png'
+}
+pwd.onblur = function(){
+  password_dl.style.border = '1px solid #ddd';
+  suo_img.src = 'img/index/suo.png'
+}
+
+var saoma_img = document.getElementById('saoma_img');
+var dianshi_img = document.getElementById("dianshi_img");
+saoma_img.onclick = function(){
+  saoma_img.src = 'img/index/dianshi.png';
+  erweima_div.style.display = 'block';
+  dl_div.style.display = 'none';
+}
+dianshi_img.onclick = function(){
+  dianshi_img.src = 'img/index/saoma.png';
+  erweima_div.style.display = 'none';
+  dl_div.style.display = 'block';
+}
